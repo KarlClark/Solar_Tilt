078ff0d27f7b0b45f7359d3690583ec9c3cbd0b0
Karl Clark <karlclark01@gmail.com>
2022-07-14 19:28:19 -0700

    add privacy policy

--- app/src/main/java/com/clarkgarrett/solartilt/Activities/PrivacyPolicyActivity.java
+++ app/src/main/java/com/clarkgarrett/solartilt/Activities/PrivacyPolicyActivity.java
@@ -2,13 +2,26 @@ package com.clarkgarrett.solartilt.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.webkit.WebView;
 
-public class PrivacyPolicyActivity extends AppCompatActivity {
+import com.clarkgarrett.solartilt.R;
+import com.clarkgarrett.solartilt.Utility;
+
+import java.nio.charset.StandardCharsets;
+
+public class PrivacyPolicyActivity extends Activity {
+
+    private String TAG = "## My Info ##";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
+        WebView wvPrivacy = findViewById(R.id.wvPrivacy);
+        wvPrivacy.loadUrl("file:///android_asset/privacy_policy.htm" );
     }
 }
